70d6008e348f9d7bff54c0eb86a6b61cec5e14e2
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2023-03-22 15:51:06 +0100

    navbar ok

---
+++ components/Container.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar/Navbar'
+
+export default function Container({ children }) {
+  return (
+    <>
+        <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Passionnée par le développement web, je crée des sites web et des applications web. "
+        />
+        <meta
+          property="og:title"
+          content="Thuc-Nhi Wiedenhofer - Développeur d'application web spécialité Front End Javascript React"
+        />
+        <meta property="og:type" content=" web developer website" />
+        <meta property="og:url" content="" />
+        <meta property="og:image" content="" />
+        <meta
+          property="og:description"
+          content=" Passionnée par le développement web, j'aimerais travailler sur votre projet de création de site web ou d'application web."
+        />
+        <title>Thuc Nhi Wiedenhofer Portfolio</title>
+      </Head>
+      <Navbar/>
+      <main>{children}</main>
+    </>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@/styles/normalize.css";
+import { Inter } from "next/font/google";
+import Container from "../components/Container";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Container className={inter.className}>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
