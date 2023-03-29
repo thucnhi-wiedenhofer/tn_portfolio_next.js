0d09970acc9b155104a0f94619b7892bb0f003fc
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2023-03-29 11:53:27 +0200

    footer ok

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,48 +1,69 @@
 import React from "react";
 import styles from "./Footer.module.css";
 import Link from "next/link";
+import Image from "next/image";
+import linkedin from "./../../public/images/icons/linkedin-icon.svg";
+import github from "./../../public/images/icons/github-icon.svg";
 
 export default function Footer() {
   return (
     <footter>
       <div className={styles.container}>
         <nav className={styles.navbar}>
-          <div>
-            <ul className={styles.nav}>
-              <li className={styles.navItem}>
-                <Link className={styles.link} aria-current="page" href="/">
-                  Accueil
-                </Link>
-              </li>
-              <li className={styles.navItem}>
-                <Link className={styles.link} aria-current="page" href="/admin">
-                  Administration
-                </Link>
-              </li>
-              <li className={styles.navItem}>
-                <Link className={styles.btn} href="/projets">
-                  Projets
-                </Link>
-              </li>
-              <li className={styles.navItem}>
-                <Link className={styles.btnOutline} href="/contact">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-            <ul className={styles.icons}>
-              <li>
-                <Link href="#" class="icon brands alt fa-github">
-                  <span class="label">GitHub</span>
-                </Link>
-              </li>
-              <li>
-                <Link href="#" class="icon brands alt fa-linkedin-in">
-                  <span class="label">LinkedIn</span>
-                </Link>
-              </li>
-            </ul>
-          </div>
+          <ul className={styles.nav}>
+            <li className={styles.navItem}>
+              <Link className={styles.link} aria-current="page" href="/">
+                Accueil
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link className={styles.link} aria-current="page" href="/admin">
+                Administration
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link className={styles.btn} href="/projets">
+                Projets
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link className={styles.btnOutline} href="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <ul className={styles.label}>
+            <li>
+              <Link
+                target="_blank"
+                href="https://github.com/thucnhi-wiedenhofer"
+              >
+                <span className={styles.icon}>
+                  <Image
+                    src={github}
+                    alt="Github logo"
+                    width="18"
+                    height="18"
+                  />
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                target="_blank"
+                href="https://www.linkedin.com/in/thuc-nhi-wiedenhofer/"
+              >
+                <span className={styles.icon}>
+                  <Image
+                    src={linkedin}
+                    alt="linkedin logo"
+                    width="18"
+                    height="18"
+                  />
+                </span>
+              </Link>
+            </li>
+          </ul>
         </nav>
       </div>
     </footter>
